97f7bb9d4e2da615268089bd725effe89c571190
Prietoalejo <>
2025-03-09 00:28:29 -0400

    DFS y BSF

--- src/proyecto1prietoa/Proyecto1PrietoA.java
+++ src/proyecto1prietoa/Proyecto1PrietoA.java
@@ -14,19 +14,22 @@ public class Proyecto1PrietoA {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-
-        CampoMinas campo = new CampoMinas(5, 5); // Crear un campo de 5x5
-    campo.marcarBombas(5); // Marcar 5 bombas
-    campo.dibujarGrafo(); // Dibujar el grafo
-
+    // Crear un campo de minas de 5x5
+    CampoMinas campo = new CampoMinas(5, 5);
+    
+    // Marcar 5 bombas en el campo
+    campo.marcarBombas(5);
+    
+    // Dibujar el estado inicial del campo
+    campo.dibujarGrafo();
+    
     // Inicializar la matriz de visitadas
     boolean[][] visitadas = new boolean[5][5];
-
-    // Simular un clic en la casilla A1
-    Adyacentes recorrido = campo.dfs(campo.matriz[0][0], visitadas); // Asumiendo que A1 es la casilla en (0, 0)
-
-    // Mostrar el recorrido
+    
+    // Simular un clic en la casilla A1 (que corresponde a matriz[0][0])
+    Adyacentes recorrido = campo.bfs(campo.matriz[0][0], visitadas);
+    
+    // Mostrar las casillas visitadas
     System.out.println("Casillas visitadas:");
     recorrido.imprimir();
 }
